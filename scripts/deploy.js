85e57623da161f378027ebe4f8a4aedb1a7446e5
Anshul Garg <anshulgarg398@gmail.com>
2024-02-12 22:05:27 +0530

    first web3 app

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const Voting = await ethers.getContractFactory("Voting");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const Voting_ = await Voting.deploy(["Mark", "Mike", "Henry", "Rock"], 90);
+  console.log("Contract address:", Voting_.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+//  async function main() {
+//    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+//    const hello_world = await HelloWorld.deploy("Hello World!");
+//    console.log("Contract Deployed to Address:", hello_world.address);
+//  }
